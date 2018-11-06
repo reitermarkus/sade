1d2401b8f9a01098399fa02277bfa0a93ee8c3af
Markus Reiter <me@reitermark.us>
2018-11-06 18:38:35 +0100

    Add project 1, week 1.

---
+++ project1/languages.py
@@ -0,0 +1,34 @@
+LANGUAGES = {
+  'python': {
+    'name': 'Python',
+    'extensions': [
+      'py',
+    ],
+  },
+  'ruby': {
+    'name': 'Ruby',
+    'extensions': [
+      'rb',
+    ],
+  },
+  'rust': {
+    'name': 'Rust',
+    'extensions': [
+      'rs',
+    ],
+  },
+  'c': {
+    'name': 'C',
+    'extensions': [
+      'c',
+      'h',
+    ],
+  },
+  'c++': {
+    'name': 'C++',
+    'extensions': [
+      'cpp',
+      'hpp',
+    ],
+  },
+}
---
+++ project1/test.rb
@@ -0,0 +1,9 @@
+# Klass Comment
+class Klass
+  # Method Comment
+  def initialize(var)
+    return if @var.nil?
+
+    @var = var
+  end
+end
