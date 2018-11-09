cc4abf8194ed7d449504756f70cde88a727fe16c
Markus Reiter <me@reitermark.us>
2018-11-09 20:21:33 +0100

    Refactoring.

--- project1/fetch_repos.py
+++ project1/fetch_repos.py
@@ -9,15 +9,14 @@ import os
 import pygount
 import time
 
-
-access_token = os.environ['GITHUB_API_TOKEN']
+access_token = os.environ['GITHUB_TOKEN']
 user = Github(access_token)
 
-MIN_LOC = 100
+MIN_LOC = 1_000_000
 loc_counter = 0
 min_loc_reached_languages = []
 
-def check_rate_limit(lower_limit=10):
+def check_rate_limit(lower_limit = 10):
   rate_limit = user.get_rate_limit()
   remaining = rate_limit.search.remaining
 
@@ -32,7 +31,6 @@ def check_rate_limit(lower_limit=10):
     print(f'Waiting {sleep_time} seconds for rate limit reset.')
     time.sleep(sleep_time)
 
-
 def check_loc_counter(language, locs_to_add):
   global loc_counter
   loc_counter += locs_to_add
@@ -40,45 +38,50 @@ def check_loc_counter(language, locs_to_add):
   if loc_counter >= MIN_LOC:
       min_loc_reached_languages.append(language)
 
-
 def analyze(r):
+  if (r.language in min_loc_reached_languages):
+    return
+
+  repo_info = {
+    'owner': r.owner.login,
+    'name': r.name,
+    'language': r.language,
+    'stars': r.stargazers_count,
+    'forks': r.forks_count,
+    'default_branch': r.default_branch,
+  }
+
   check_rate_limit()
 
   extensions = [LANGUAGES[lang]['extensions'] for lang in LANGUAGES if LANGUAGES[lang]['name'] == r.language][0]
 
-  with Repo(r.owner.login, r.name, r.language, extensions) as repo:
-    if (r.language in min_loc_reached_languages):
-      return
-
+  with Repo(r.owner.login, r.name, default_branch = r.default_branch, language = r.language, extensions = extensions) as repo:
     analysis = [pygount.source_analysis(file, repo.language) for file in repo.files]
     analysis = [a for a in analysis if a.state == 'analyzed']
 
-    analysis = [
-      np.array([a.code + a.string, a.documentation, a.empty])
-      for a in analysis
-    ]
+    if analysis:
+      analysis = [
+        np.array([a.code + a.string, a.documentation, a.empty])
+        for a in analysis
+      ]
+    else:
+      analysis = [np.array([0, 0, 0])]
 
-    info = {
-      'repo': repo.repo,
-      'language': repo.language
-    }
-
-    info['code'], info['documentation'], info['empty'] = tuple(sum(analysis))
+    repo_info['code'], repo_info['documentation'], repo_info['empty'] = tuple(sum(analysis))
 
     check_loc_counter(r.language, sum(sum(analysis)))
-    
-    print(info)
 
+    print(repo_info)
 
-def search(language, stars, forks, sort_by, order):
-  repos = user.search_repositories(f'language:{language}', sort_by, order, stars=stars, forks=forks, created=">=2018-11-01")
+def search(language):
+  repos = user.search_repositories(f'language:{language}', 'stars', 'desc', stars = '>=100', forks = '>=10')
   return [analyze(repo) for repo in repos]
-
 
 if __name__ == '__main__':
   try:
-    for lang in LANGUAGES:
-      search(LANGUAGES[lang]['name'], '>=10', '>=10', 'stars', 'desc')
+    for language in LANGUAGES:
+      print(f'Searching {language}:')
+      search(language)
       loc_counter = 0
   except GithubException as e:
     print(e)
