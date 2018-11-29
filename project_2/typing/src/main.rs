75eaa34cdbfe3c9dddd611750c1c919f028f98db
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-11-29 23:05:32 +0100

    add rust project; read json files

---
+++ project_2/typing/src/main.rs
@@ -0,0 +1,46 @@
+use std::fs;
+use std::path::Path;
+use std::result::Result;
+use std::ffi::OsStr;
+use std::error::Error;
+
+extern crate serde_json;
+
+fn is_json(path: &Path) -> bool {
+    let extension = path.extension().and_then(OsStr::to_str).unwrap();
+
+    match extension {
+        "json" => true,
+        _ => false,
+    }
+}
+
+fn read<P: AsRef<Path>>(path: P) -> Result<serde_json::Value, Box<Error>> {
+    let file = fs::File::open(path)?;
+    let json = serde_json::from_reader(file)?;
+
+    Result::Ok(json)
+}
+
+fn iterate_over_files(path: &Path) -> std::io::Result<()> {
+    for entry in fs::read_dir(path)? {
+        let path = &entry?.path();
+
+        if is_json(path) {
+            match read(path) {
+                Ok(_) => println!("{:?} read successfully", path),
+                Err(_) => println!("{:?} can not be read", path),
+            }
+        }
+    }
+
+    Ok(())
+}
+
+fn main() -> std::io::Result<()> {
+    let group_a = &"../data/Collected Data/group_a/";
+
+    iterate_over_files(Path::new(group_a))?;
+
+    Ok(())
+}
