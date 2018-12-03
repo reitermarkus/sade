acb773494dc56b8024af5125f2fee57ce0703725
H.Gndgn <hueseyin.guendogan@gmail.com>
2018-12-03 11:39:25 +0100

    fix spaces

--- project_2/typing/src/main.rs
+++ project_2/typing/src/main.rs
@@ -17,100 +17,100 @@ extern crate serde_json;
 use serde_json::Value;
 
 fn is_json(path: &Path) -> bool {
-	let extension = path.extension().and_then(OsStr::to_str).unwrap();
+  let extension = path.extension().and_then(OsStr::to_str).unwrap();
 
-	extension == "json"
+  extension == "json"
 }
 
 fn read(path: &Path) -> Result<Value, Box<Error>> {
-	let file = fs::File::open(path)?;
-	let json = serde_json::from_reader(file)?;
+  let file = fs::File::open(path)?;
+  let json = serde_json::from_reader(file)?;
 
-	Result::Ok(json)
+  Result::Ok(json)
 }
 
 fn create_missing_data_file(group: &str, missing_data: &Vec<MissingTasks>) {
-	let mut f = OpenOptions::new()
-		.write(true)
-		.create_new(true)
-		.open(Path::new(&format!("../missing_tasks_{}.json", group)))
-		.unwrap();
+  let mut f = OpenOptions::new()
+    .write(true)
+    .create_new(true)
+    .open(Path::new(&format!("../missing_tasks_{}.json", group)))
+    .unwrap();
 
-	let data = serde_json::to_string(&missing_data).unwrap();
-	f.write(data.as_bytes()).unwrap();
+  let data = serde_json::to_string(&missing_data).unwrap();
+  f.write(data.as_bytes()).unwrap();
 }
 
 fn find(
-	path: &Path,
-	group: &str,
-	tasks: &Vec<String>,
-	missing_data: &mut Vec<MissingTasks>,
+  path: &Path,
+  group: &str,
+  tasks: &Vec<String>,
+  missing_data: &mut Vec<MissingTasks>,
 ) -> std::io::Result<()> {
-	for entry in fs::read_dir(path)? {
-		let path = &entry?.path();
+  for entry in fs::read_dir(path)? {
+    let path = &entry?.path();
 
-		if is_json(path) {
-			match read(path) {
-				Ok(json) => {
-					let user = path
-						.file_stem()
-						.and_then(OsStr::to_str)
-						.unwrap()
-						.to_string();
+    if is_json(path) {
+      match read(path) {
+        Ok(json) => {
+          let user = path
+            .file_stem()
+            .and_then(OsStr::to_str)
+            .unwrap()
+            .to_string();
 
-					check_tasks(user, group, &tasks, json, missing_data);
-				}
-				Err(_) => println!("{:?} can not be read", path),
-			}
-		}
-	}
+          check_tasks(user, group, &tasks, json, missing_data);
+        }
+        Err(_) => println!("{:?} can not be read", path),
+      }
+    }
+  }
 
-	Ok(())
+  Ok(())
 }
 
 fn check_tasks(
-	user: String,
-	group: &str,
-	tasks: &Vec<String>,
-	user_data: Value,
-	missing_data: &mut Vec<MissingTasks>,
+  user: String,
+  group: &str,
+  tasks: &Vec<String>,
+  user_data: Value,
+  missing_data: &mut Vec<MissingTasks>,
 ) {
-	let mut missing = vec![];
+  let mut missing = vec![];
 
-	for i in tasks.iter() {
-		let task: String = format!("{0}{1}", group, i.to_string());
+  for i in tasks.iter() {
+    let task: String = format!("{0}{1}", group, i.to_string());
 
-		if user_data.get(&task) == None {
-			missing.push(task);
-		}
-	}
+    if user_data.get(&task) == None {
+      missing.push(task);
+    }
+  }
 
-	if missing.len() > 0 {
-		missing_data.push(MissingTasks {
-			user: user,
-			missing: missing,
-		});
-	}
+  if missing.len() > 0 {
+    missing_data.push(MissingTasks {
+      user: user,
+      missing: missing,
+    });
+  }
 }
 
 fn find_missing_tasks(path: &Path, group_name: &str, tasks: &Vec<String>) {
-	let mut missing_tasks: Vec<MissingTasks> = Vec::new();
+  let mut missing_tasks: Vec<MissingTasks> = Vec::new();
 
-	find(path, group_name, &tasks, &mut missing_tasks).unwrap();
+  find(path, group_name, &tasks, &mut missing_tasks).unwrap();
 
-	create_missing_data_file(group_name, &missing_tasks);
+  create_missing_data_file(group_name, &missing_tasks);
 }
 
 fn main() {
-	find_missing_tasks(
-		Path::new("../data/Collected Data/group_a/"),
-		"group_a",
-		&meta_info::tasks_group_a(),
-	);
+  find_missing_tasks(
+    Path::new("../data/Collected Data/group_a/"),
+    "group_a",
+    &meta_info::tasks_group_a(),
+  );
 
-	find_missing_tasks(
-		Path::new("../data/Collected Data/group_b/"),
-		"group_b",
-		&meta_info::tasks_group_b(),
-	);
+  find_missing_tasks(
+    Path::new("../data/Collected Data/group_b/"),
+    "group_b",
+    &meta_info::tasks_group_b(),
+  );
 }
