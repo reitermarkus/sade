ebb0acf524a7f309da5a1cd688d052fb4664e1e6
Markus Reiter <me@reitermark.us>
2018-12-04 17:17:31 +0100

    Refactor analysis.

--- project_2/typing/src/main.rs
+++ project_2/typing/src/main.rs
@@ -1,20 +1,20 @@
 use std::error::Error;
-use std::ffi::OsStr;
-use std::fs;
+use std::fs::File;
 use std::fs::OpenOptions;
 use std::io::Write;
-use std::path::Path;
+use std::path::{Path, PathBuf};
 use std::result::Result;
-
-mod meta_info;
-use meta_info::*;
+use std::collections::HashMap;
 
 #[macro_use]
 extern crate serde_derive;
 extern crate serde;
 extern crate serde_json;
-
 use serde_json::Value;
+use serde::ser::Serialize;
+
+extern crate glob;
+use glob::glob;
 
 #[derive(Serialize, Deserialize, Debug)]
 struct TaskInfo {
@@ -29,222 +29,129 @@ struct DeleteKeyInfo {
   task_infos: Vec<TaskInfo>,
 }
 
-// General Purpose
-fn is_json(path: &Path) -> bool {
-  path.extension() == Some(OsStr::new("json"))
-}
-fn read(path: &Path) -> Result<Value, Box<Error>> {
-  let file = fs::File::open(path)?;
-  let json = serde_json::from_reader(file)?;
-
-  Result::Ok(json)
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ModifyErrorRangeInner {
+  line: u64,
+  character: u64,
 }
 
-fn extract_file_stem(path: &Path) -> String {
-  path
-    .file_stem()
-    .and_then(OsStr::to_str)
-    .unwrap()
-    .to_owned()
-    .to_string()
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ModifyErrorRange {
+  start: ModifyErrorRangeInner,
+  end: ModifyErrorRangeInner,
 }
 
-// Delete Key Analysis
-fn delete_key_analysis(path: &Path, group: String) -> std::io::Result<()> {
-  let mut del_key_infos: Vec<DeleteKeyInfo> = Vec::new();
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ModifyError {
+  severity: u64,
+  range: ModifyErrorRange,
+  message: String,
+}
 
-  for entry in fs::read_dir(path)? {
-    let path = &entry?.path();
+#[derive(Serialize, Deserialize, Debug)]
+pub struct ModifyStep {
+  file: String,
+  errors: Vec<ModifyError>,
+  timestamp: u64,
+}
 
-    if is_json(path) {
-      match read(path) {
-        Ok(json) => {
-          let ref user = extract_file_stem(path);
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(untagged)]
+pub enum Task {
+  Questionnaire(HashMap<String, Value>),
+  Task(Vec<ModifyStep>)
+}
 
-          del_key_infos.push(analyze_json(&user.to_string(), &json));
+fn count_del_keys(steps: &Vec<ModifyStep>) -> usize {
+  let mut presses = 0;
+  let mut prev_len = None;
 
-          println!("group_{}_{} analyzed", group, user);
-        }
-        Err(_) => println!("{:?} can not be read", path),
+  for step in steps {
+    let curr_len = step.file.len();
+
+    if let Some(prev_len) = prev_len {
+      if curr_len < prev_len {
+        presses += prev_len - curr_len;
       }
     }
+
+    prev_len = Some(curr_len);
   }
 
-  let p = &format!("../delete_key_analysis_{}.json", group);
+  presses
+}
 
-  let mut file;
+fn user_info_for_group(data_path: impl AsRef<Path>, group: &str) -> Result<Vec<PathBuf>, Box<Error>> {
+  let paths = glob(&format!("{}/group_{}/user_*.json", data_path.as_ref().to_str().unwrap(), group))?
+                .filter_map(|path| path.ok())
+                .map(|path| PathBuf::from(path))
+                .collect();
 
-  if Path::new(p).exists() {
-    file = OpenOptions::new().write(true).open(p).unwrap();
-  } else {
-    file = OpenOptions::new()
-      .write(true)
-      .create_new(true)
-      .open(p)
-      .unwrap();
+  Ok(paths)
+}
+
+fn tasks_for_group(data_path: impl AsRef<Path>, group: &str) -> Result<Vec<String>, Box<Error>> {
+  let meta_path = data_path.as_ref().join(format!("group_{}", group)).join(format!("exp_group_{}.meta", group));
+
+  let file = File::open(meta_path)?;
+
+  let meta: HashMap<String, Value> = serde_json::from_reader(&file)?;
+
+  Ok(meta.get("tasks")
+      .and_then(|tasks| tasks.as_object())
+      .map(|tasks| tasks.keys().map(|s| s.to_owned()).collect())
+      .unwrap_or(Vec::new()))
+}
+
+fn analyze_group(data_path: impl AsRef<Path>, group: &str) -> Result<HashMap<String, usize>, Box<Error>> {
+  let tasks = tasks_for_group(&data_path, group)?;
+
+  let mut user_infos: Vec<HashMap<String, Task>> = Vec::new();
+
+  for user_info in user_info_for_group(&data_path, group)? {
+    let file = File::open(user_info)?;
+
+    user_infos.push(serde_json::from_reader(&file)?);
   }
 
-  let data = serde_json::to_string_pretty(&del_key_infos)
-    .unwrap()
-    .as_bytes()
-    .to_owned();
+  Ok(tasks.iter().map(|task| {
+    let delete_key_presses = user_infos.iter().map(|user_info| {
+      if let Some(Task::Task(steps)) = user_info.get(task) {
+        count_del_keys(&steps)
+      } else {
+        0
+      }
+    }).sum();
 
-  file.write_all(&data).unwrap();
+    (task.to_owned(), delete_key_presses)
+  }).collect())
+}
+
+fn main() -> Result<(), Box<Error>> {
+  let data_path = "../data/Collected Data";
+
+  let group_a = analyze_group(data_path, "a")?;
+  let group_b = analyze_group(data_path, "b")?;
+
+  write_json("../analysis_group_a.json", &group_a)?;
+  write_json("../analysis_group_b.json", &group_b)?;
 
   Ok(())
 }
 
-fn analyze_json(user: &String, user_data: &Value) -> DeleteKeyInfo {
-  let mut del_info = DeleteKeyInfo {
-    user: user.to_string(),
-    total_pressed: 0,
-    task_infos: vec![],
-  };
+fn write_json<T: ?Sized>(path: impl AsRef<Path>, data: &T) -> Result<(), Box<Error>>
+where T: Serialize
+{
+  let mut file = OpenOptions::new()
+                   .write(true)
+                   .create_new(true)
+                   .open(path)?;
 
-  let mut task_info = TaskInfo {
-    name: "".to_string(),
-    del_key_pressed: 0,
-  };
+  let data = serde_json::to_string_pretty(data)?
+               .as_bytes()
+               .to_owned();
 
-  match user_data.as_object() {
-    None => panic!("user data is None"),
-    Some(m) => {
-      for i in m.keys() {
-        let mut task: &str = i;
-
-        task_info.name = i.to_string();
-
-        if is_modify_task(&task.to_string()) {
-          match count_delete_keys(&user_data, task) {
-            Ok(res) => {
-              task_info = res;
-              del_info.total_pressed += task_info.del_key_pressed;
-              del_info.task_infos.push(task_info);
-            }
-            Err(_) => (),
-          }
-        }
-      }
-    }
-  }
-
-  del_info
-}
-
-fn count_delete_keys(user_data: &Value, task: &str) -> Result<TaskInfo, ()> {
-  let data = user_data.get(task).unwrap().as_array().unwrap();
-
-  if data.len() == 0 {
-    return Err(());
-  };
-
-  let mut previous_file: String = data[0]["file"].to_owned().to_string();
-
-  let mut task_info = TaskInfo {
-    name: task.to_string(),
-    del_key_pressed: 0,
-  };
-
-  for i in 1..data.len() {
-    let current_file = data[i]["file"].to_owned().to_string();
-
-    if current_file.len() < previous_file.len() {
-      task_info.del_key_pressed += previous_file.len() - current_file.len();
-    }
-
-    previous_file = data[i]["file"].to_owned().to_string();
-  }
-
-  Ok(task_info)
-}
-
-fn is_modify_task(task_name: &String) -> bool {
-  task_name.ends_with("modify")
-}
-
-// Find Missing Tasks
-fn create_missing_data_file(group: &str, missing_data: &Vec<MissingTasks>) {
-  let p = &format!("../missing_tasks_{}.json", group);
-  let mut file;
-
-  if Path::new(p).exists() {
-    file = OpenOptions::new().write(true).open(p).unwrap();
-  } else {
-    file = OpenOptions::new()
-      .write(true)
-      .create_new(true)
-      .open(p)
-      .unwrap();
-  }
-
-  let data = serde_json::to_string_pretty(&missing_data).unwrap();
-  file.write_all(data.as_bytes()).unwrap();
-}
-
-fn find(
-  path: &Path,
-  group: &str,
-  tasks: &Vec<String>,
-  missing_data: &mut Vec<MissingTasks>,
-) -> std::io::Result<()> {
-  for entry in fs::read_dir(path)? {
-    let path = &entry?.path();
-
-    if is_json(path) {
-      match read(path) {
-        Ok(json) => {
-          let ref user = extract_file_stem(path);
-
-          check_tasks(&user.to_string(), group, &tasks, json, missing_data);
-        }
-        Err(_) => println!("{:?} can not be read", path),
-      }
-    }
-  }
+  file.write_all(&data)?;
 
   Ok(())
-}
-
-fn check_tasks(
-  user: &String,
-  group: &str,
-  tasks: &Vec<String>,
-  user_data: Value,
-  missing_data: &mut Vec<MissingTasks>,
-) {
-  let mut missing = vec![];
-
-  for i in tasks.iter() {
-    let task: String = format!("{0}{1}", group, i.to_string());
-
-    if user_data.get(&task) == None {
-      missing.push(task);
-    }
-  }
-
-  if missing.len() > 0 {
-    missing_data.push(MissingTasks {
-      user: user.to_string(),
-      missing: missing,
-    });
-  }
-}
-
-fn find_missing_tasks(path: &Path, group_name: &str, tasks: &Vec<String>) {
-  let mut missing_tasks: Vec<MissingTasks> = Vec::new();
-
-  find(path, group_name, &tasks, &mut missing_tasks).unwrap();
-
-  create_missing_data_file(group_name, &missing_tasks);
-}
-
-fn main() {
-  let path_a = Path::new("../data/Collected Data/group_a/");
-  let path_b = Path::new("../data/Collected Data/group_b/");
-
-  delete_key_analysis(path_a, "a".to_string()).unwrap();
-  delete_key_analysis(path_b, "b".to_string()).unwrap();
-
-  find_missing_tasks(path_a, "a", &meta_info::tasks_group_a());
-  find_missing_tasks(path_b, "b", &meta_info::tasks_group_b());
 }
