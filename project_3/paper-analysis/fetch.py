14649e4706f760be86d7a023bae026273924b935
Markus Reiter <me@reitermark.us>
2019-01-07 18:42:49 +0100

    Refactor fetch script.

--- project_3/paper-analysis/fetch.py
+++ project_3/paper-analysis/fetch.py
@@ -1,17 +1,24 @@
-# /usr/bin/python
+#!/usr/bin/env python3
+
 import requests
 import json
+import urllib
 
-papers_path = '../data/papers.json'
+papers_path = 'data/papers.json'
 
 api_endpoint = 'http://ieeexploreapi.ieee.org/api/v1/search/articles'
 api_key = '2quujqxvyzwpzztgkud8w2x6'
-content_type = "Conferences%2C+Journals+%26+Magazines"
-query_text = 'Domain+Specific+Languages'
-start_year = '2010'
-max_records = 100
 
-url = f'{api_endpoint}?apikey={api_key}&format=json&content_type={content_type}&querytext={query_text}&start_year={start_year}&max_records={max_records}'
+query_string = urllib.parse.urlencode({
+  'apikey': api_key,
+  'format': 'json',
+  'content_type': 'Conferences, Journals & Magazines',
+  'querytext': 'Domain Specific Languages',
+  'start_year': 2010,
+  'max_records': 100,
+})
+
+url = f'{api_endpoint}?{query_string}'
 res = requests.get(url=url)
 data = json.loads(res.text)
 
