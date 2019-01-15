680b3236e26efc238be7cf2e45882470245dfd68
Markus Reiter <me@reitermark.us>
2019-01-15 20:39:03 +0100

    Sort papers by title.

--- project_3/paper-analysis/fetch.py
+++ project_3/paper-analysis/fetch.py
@@ -10,25 +10,23 @@ api_endpoint = 'http://ieeexploreapi.ieee.org/api/v1/search/articles'
 api_key = '2quujqxvyzwpzztgkud8w2x6'
 
 query_string = urllib.parse.urlencode({
-    'apikey': api_key,
-    'format': 'json',
-    'content_type': 'Conferences, Journals & Magazines',
-    'querytext': 'Domain Specific Languages',
-    'start_year': 2010,
-    'max_records': 100,
+  'apikey': api_key,
+  'format': 'json',
+  'content_type': 'Conferences, Journals & Magazines',
+  'querytext': 'Domain Specific Languages',
+  'start_year': 2010,
+  'max_records': 100,
 })
 
 url = f'{api_endpoint}?{query_string}'
 res = requests.get(url=url)
 data = json.loads(res.text)
 
-
 def clean_data(data):
-  return list(map(lambda a: {
-      'title': a.get('title'),
-      'abstract': a.get('abstract')
-  }, data['articles']))
-
+  return sorted(map(lambda a: {
+    'title': a['title'],
+    'abstract': a['abstract'],
+  }, data['articles']), key = lambda a: a['title'])
 
 with open(papers_path, 'w+', encoding = 'utf-8') as f:
   json.dump(clean_data(data), f, indent = 2)
