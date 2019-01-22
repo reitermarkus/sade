2d245dc8af8877dbc769dbb9d2bab23c79b25d22
Markus Reiter <me@reitermark.us>
2019-01-22 10:25:36 +0100

    Improve normalization.

--- project_3/normalize.py
+++ project_3/normalize.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import re
+
 from nltk.corpus import stopwords
 from nltk.stem.wordnet import WordNetLemmatizer
 from nltk.tokenize import word_tokenize
@@ -11,7 +13,16 @@ exclude = set(string.punctuation)
 lemma = WordNetLemmatizer()
 
 def clean(text):
-  text = ''.join(' ' if c is '-' else c for c in text.lower())
+  text = text.lower()
+
+  # Canonicalize DSL.
+  text = re.sub(r'\bdomainspecific\b', 'domain-specific', text)
+  text = re.sub(r'\bdsl\b', 'domain-specific language', text)
+
+  # Remove “'s”.
+  text = re.sub(r"'s\b", ' ', text)
+
+  text = ''.join(' ' if c is '-' else c for c in text)
   tokens = word_tokenize(text)
   punc_free = [w for w in tokens if not w in exclude]
   lemmas = [lemma.lemmatize(word) for word in punc_free]
